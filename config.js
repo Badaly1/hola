c40220712d91791f83aa3cc6259b890d7fd230b6
Ahmadsholah <ahmadsholahudin2254@gmail.com>
2024-08-05 01:42:09 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+
+const CONFIG = {
+    titleWeb: "Halo sayangg",
+    introTitle: 'Amanda Soebandini',
+    introDesc: `Kenapa kamu begitu cantik? Rasanya setiap hari aku menemukan hal baru tentang dirimu yang membuatku semakin terpesona. Kamu tuh punya pesona yang sulit dijelaskan dengan kata-kata.`,
+    btnIntro: 'Alowwww',
+    title: 'Apakah mungkin kamu merasakan sesuatu yang lebih dari sekadar suka padaku? ? 🥰',
+    desc: 'Karena aku selalu bertanya-tanya, apakah ada rasa sayang yang tersimpan di dalam hatimu untukku?"',
+    btnYes: 'Iyaaa<33',
+    btnNo: 'Enggaa:3',
+    question: 'Di dunia ini ada lebih dari 7 miliar orang, kenapa kamu malah jatuh cinta padaku <3?',
+    btnReply: 'Kirimmm<3',
+    reply: 'Karna Aku Sayang Banget Sama Kamuu',
+    mess: 'Aku tahu kok 🥰. Aku juga sangat sayang kamu 😘😘',
+    messDesc: 'Aku minta maaf yaa belum bisa ajak kamu mainn😔.',
+    btnAccept: 'iyaaaa<3',
+    messLink: 'https://badaly1.github.io/holaa/'
+}
